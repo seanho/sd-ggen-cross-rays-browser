19de91bcff0a304aaed40fb080196a8c02d8fefe
Sean Ho <sean@guru.hk>
2020-05-25 09:32:18 +1000

    Use GitHub Event to extract PR number

--- .github/scripts/update_bundle_version.sh
+++ .github/scripts/update_bundle_version.sh
@@ -2,7 +2,6 @@
 
 set -eo pipefail
 
-PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
 BUNDLE_SHORT_VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "./CrossRaysUnitBrowser/Info.plist")
 BUNDLE_VERSION="$BUNDLE_SHORT_VERSION.$PR_NUMBER"
 
