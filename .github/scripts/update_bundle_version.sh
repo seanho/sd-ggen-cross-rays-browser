4940e3af9655e45f3cc7e6f23eac8ab56a5b59b7
Sean Ho <sean@guru.hk>
2020-05-25 08:51:33 +1000

    Add update bundle version script

---
+++ .github/scripts/update_bundle_version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -eo pipefail
+
+BUNDLE_SHORT_VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "./CrossRaysUnitBrowser/Info.plist")
+BUNDLE_VERSION="$BUNDLE_SHORT_VERSION.$PR_NUMBER"
+
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUNDLE_VERSION" "./CrossRaysUnitBrowser/Info.plist"
