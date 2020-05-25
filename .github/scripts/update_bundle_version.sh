18ea188d954b2d2c0b48890bc8835c9e26fccd82
Sean Ho <sean@guru.hk>
2020-05-25 10:16:10 +1000

    Use run number as build number

--- .github/scripts/update_bundle_version.sh
+++ .github/scripts/update_bundle_version.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 BUNDLE_SHORT_VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "./CrossRaysUnitBrowser/Info.plist")
-BUNDLE_VERSION="$BUNDLE_SHORT_VERSION.$PR_NUMBER"
+BUNDLE_VERSION="$BUNDLE_SHORT_VERSION.$RUN_NUMBER"
 
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUNDLE_VERSION" "./CrossRaysUnitBrowser/Info.plist"
